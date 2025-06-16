7a4597c91f495251efbc0f3898efa293bb07849b
taotaoxiaowai <by15700775391@126.com>
2025-06-16 16:21:49 +0800

    添加类注释创建用户类

--- src/main/java/com/neusoft/SP01/controller/GoOutController.java
+++ src/main/java/com/neusoft/SP01/controller/GoOutController.java
@@ -1,4 +1,11 @@
 package com.neusoft.SP01.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RequestMapping("/GoOutController")
+@RestController
 public class GoOutController {
 }
--- src/main/java/com/neusoft/SP01/controller/UserController.java
+++ src/main/java/com/neusoft/SP01/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("*")
-@RequestMapping("/CheckOutController")
+@RequestMapping("/UserController")
 @RestController
-public class CheckOutController {
+public class UserController {
 }
