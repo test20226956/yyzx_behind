d2cc467c81b0103a55d939be996069bbc40e3911
HUAWEI <3293273117@qq.com>
2025-06-16 16:10:45 +0800

    meal->UserDao

---
+++ src/main/java/com/neusoft/SP01/dao/MealDao.java
@@ -0,0 +1,11 @@
+package com.neusoft.SP01.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 对应t_meal
+ */
+@Mapper
+public interface MealDao {
+
+}
---
+++ src/main/java/com/neusoft/SP01/dao/OutRecordDao.java
@@ -0,0 +1,10 @@
+package com.neusoft.SP01.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 对应t_out_record
+ */
+@Mapper
+public interface OutRecordDao {
+}
